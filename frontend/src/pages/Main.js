904623828c1aefa7b4c2907ff35d8e22d1f18d6e
gabrielNetto94 <silveiranetto94@gmail.com>
2019-08-08 16:40:58 -0300

    estilizando o main

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import './Main.css';
+
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main({ match }){
+    return(
+        <div className="main-container">
+            <img src={logo} alt="Tindev"/>
+            <ul>
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/33572929?v=4" alt=""/>
+                    <footer>
+                        <strong>Gabriel Netto</strong>
+                        <p>Programador!</p>
+                    </footer>
+
+                    <div className="buttons">
+                        
+                        <button type="button">
+                            <img src={dislike} alt="Dislike"/>
+                        </button>
+
+                        <button type="button">
+                            <img src={like} alt="Like"/>
+                        </button>
+
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/33572929?v=4" alt=""/>
+                    <footer>
+                        <strong>Gabriel Netto</strong>
+                        <p>Programador!</p>
+                    </footer>
+
+                    <div className="buttons">
+                        
+                        <button type="button">
+                            <img src={dislike} alt="Dislike"/>
+                        </button>
+
+                        <button type="button">
+                            <img src={like} alt="Like"/>
+                        </button>
+
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars0.githubusercontent.com/u/33572929?v=4" alt=""/>
+                    <footer>
+                        <strong>Gabriel Netto</strong>
+                        <p>Programador!</p>
+                    </footer>
+
+                    <div className="buttons">
+                        
+                        <button type="button">
+                            <img src={dislike} alt="Dislike"/>
+                        </button>
+
+                        <button type="button">
+                            <img src={like} alt="Like"/>
+                        </button>
+
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
